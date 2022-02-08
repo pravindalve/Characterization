170b020948f1dd05a506cf6ddcd325e80d1f85c3
DanWBR <danielwag@gmail.com>
2022-02-08 09:41:37 -0400

    setup python initialization and installation of numpy/scipy

---
+++ Characterization/Main.vb
@@ -0,0 +1,19 @@
+﻿Imports System.IO
+Imports Python.Included
+Imports Python.Runtime
+
+Module Main
+
+    Sub Main()
+
+        Installer.SetupPython().GetAwaiter().GetResult()
+        Installer.InstallWheel(Reflection.Assembly.GetExecutingAssembly(), "numpy-1.20.0-cp37-cp37m-win_amd64.whl").GetAwaiter().GetResult()
+        Installer.InstallWheel(Reflection.Assembly.GetExecutingAssembly(), "scipy-1.7.0-cp37-cp37m-win_amd64.whl").GetAwaiter().GetResult()
+        PythonEngine.Initialize()
+        Dim sys As Object = PythonEngine.ImportModule("sys")
+        Dim numpy As Object = Py.Import("numpy")
+        Dim scipy As Object = Py.Import("scipy")
+
+    End Sub
+
+End Module
