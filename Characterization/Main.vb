36db7b55881ecf5d7ccecd71bef626050a4b29d5
DanWBR <danielwag@gmail.com>
2022-02-09 12:17:28 -0400

    test code, get results

--- Characterization/Main.vb
+++ Characterization/Main.vb
@@ -30,9 +30,40 @@ Module Main
 
                 scope.Exec(charact)
 
+                Dim np As Object = scope.Import("numpy")
                 Dim charac1 As Object = scope.Get("characterization")
 
-                Dim result = charac1.characterization()
+                Dim SG = 0.809
+                Dim x = np.array(New List(Of Double)({2.9, 7.68859754431644, 15.1538050381994, 31.5823352252608, 42.4062967988428, 52.8464767354692, 62.7377406502837, 71.2394217843979, 74.0267929538375, 83.4788183586731, 88.4543077863666, 92.486636460126}))
+                Dim y = np.array(New List(Of Double)({15, 65, 100, 150, 200, 250, 300, 350, 370, 450, 500, 550}))
+
+                Dim result As Object = charac1(SG, x, y)
+
+                'return [Ti, Tf, Wf, Vf, Mf, MW, SG, Tb]
+
+                Dim TiP As Object = result(0)
+                Dim TfP As Object = result(1)
+                Dim WfP As Object = result(2)
+                Dim VfP As Object = result(3)
+                Dim MfP As Object = result(4)
+                Dim MWP As Object = result(5)
+                Dim SGP As Object = result(6)
+                Dim TbP As Object = result(7)
+
+                Dim Ti, Tf, Wf, Vf, Mf, MW, SG2, Tb As New List(Of Double)
+
+                For i As Integer = 0 To DirectCast(TiP, PyObject).Length - 1
+                    Ti.Add(Double.Parse(TiP.Item(i).ToString(), Globalization.CultureInfo.InvariantCulture))
+                    Tf.Add(Double.Parse(TfP.Item(i).ToString(), Globalization.CultureInfo.InvariantCulture))
+                    Wf.Add(Double.Parse(WfP.Item(i).ToString(), Globalization.CultureInfo.InvariantCulture))
+                    Vf.Add(Double.Parse(VfP.Item(i).ToString(), Globalization.CultureInfo.InvariantCulture))
+                    Mf.Add(Double.Parse(MfP.Item(i).ToString(), Globalization.CultureInfo.InvariantCulture))
+                    MW.Add(Double.Parse(MWP.Item(i).ToString(), Globalization.CultureInfo.InvariantCulture))
+                    SG2.Add(Double.Parse(SGP.Item(i).ToString(), Globalization.CultureInfo.InvariantCulture))
+                    Tb.Add(Double.Parse(TbP.Item(i).ToString(), Globalization.CultureInfo.InvariantCulture))
+                Next
+
+                Console.ReadKey()
 
             End Using
 
--- Characterization/characterization.py
+++ Characterization/characterization.py
@@ -3,220 +3,216 @@ from math import log
 import pandas as pd
 import numpy as np
 from scipy.optimize import curve_fit
-from math import exp, log
+from math import exp, log, ceil, floor
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 
-class functions:
+def create_cuts(IBP, FBP):
+	cuts_i = []
+	IBP = IBP - 273.15
+	FBP = FBP - 273.15
+	s = ceil(IBP / 10)
+	s = s * 10
+	if FBP > 460:
+		cuts_i += list(np.linspace(s, 460, int((460 - s) / 10) + 1))
+	else:
+		cuts_i += np.linspace(s, floor(FBP / 10) * 10, int((floor(FBP / 10) * 10 - s) / 10) + 1)
 
-	def create_cuts(IBP, FBP):
-		cuts_i = []
-		IBP = IBP - 273.15
-		FBP = FBP - 273.15
-		s = ceil(IBP / 10)
-		s = s * 10
-		if FBP > 460:
-			cuts_i += list(np.linspace(s, 460, int((460 - s) / 10) + 1))
-		else:
-			cuts_i += np.linspace(s, floor(FBP / 10) * 10, int((floor(FBP / 10) * 10 - s) / 10) + 1)
+	if FBP > 600:
+		cuts_i += list(np.linspace(480, 600, int((600 - 480) / 20)+1))
+	elif FBP > 460:
+		cuts_i += list(np.linspace(480, floor(FBP / 20) * 20, int((floor(FBP / 20) * 20 - 460) / 20)))
 
-		if FBP > 600:
-			cuts_i += list(np.linspace(480, 600, int((600 - 480) / 20)+1))
-		elif FBP > 460:
-			cuts_i += list(np.linspace(480, floor(FBP / 20) * 20, int((floor(FBP / 20) * 20 - 460) / 20)))
+	if FBP > 600:
+		cuts_i += list(np.linspace(625, 600 + floor((FBP - 600) / 25) * 25, int((floor(FBP / 25) * 25 - 600) / 25 )))
 
-		if FBP > 600:
-			cuts_i += list(np.linspace(625, 600 + floor((FBP - 600) / 25) * 25, int((floor(FBP / 25) * 25 - 600) / 25 )))
+	cuts_f = []
+	for i in range(1, len(cuts_i)):
+		cuts_f.append(cuts_i[i])
 
-		cuts_f = []
-		for i in range(1, len(cuts_i)):
-			cuts_f.append(cuts_i[i])
+	del cuts_i[-1]
 
-		del cuts_i[-1]
+	columns = ["cuts_i", "cuts_f", "Tb", "SG", "wt%", "V%", "M%", "MW", "Tc", "Pc", "Tbr", "AF"]
 
-		columns = ["cuts_i", "cuts_f", "Tb", "SG", "wt%", "V%", "M%", "MW", "Tc", "Pc", "Tbr", "AF"]
+	df = pd.DataFrame(index=range(len(cuts_i)), columns=columns)
+	df["cuts_i"] = cuts_i
+	df["cuts_f"] = cuts_f
 
-		df = pd.DataFrame(index=range(len(cuts_i)), columns=columns)
-		df["cuts_i"] = cuts_i
-		df["cuts_f"] = cuts_f
+	df["cuts_i"] += 273.15
+	df["cuts_f"] += 273.15
 
-		df["cuts_i"] += 273.15
-		df["cuts_f"] += 273.15
+	return df
 
-		return df
+def calc_MW(Tb, SG):
+	# implementation of riazi daubert correlation
+	MW = np.empty(len(Tb))
+	for i in range(len(Tb)):
+		MW[i] = 42.965*(exp(2.097*1e-4*(Tb[i]) - 7.78712*SG[i] + 2.08476*1e-3*(Tb[i])*SG[i]))*(Tb[i])**1.26007 * SG[i]**4.98308
 
-	def calc_MW(Tb, SG):
-		# implementation of riazi daubert correlation
-		MW = np.empty(len(Tb))
-		for i in range(len(Tb)):
-			MW[i] = 42.965*(exp(2.097*1e-4*(Tb[i]) - 7.78712*SG[i] + 2.08476*1e-3*(Tb[i])*SG[i]))*(Tb[i])**1.26007 * SG[i]**4.98308
+	return MW
 
-		return MW
+def calc_Tc(SG, Tb):
+	# This function implements Lee-Kesler method for calculating Tc
+	Tc = np.empty(len(SG))
+	for i in range(len(SG)):
+		Tc[i] = 189.8 + 450.6* SG[i] + (0.4244 + 0.1174*SG[i])*Tb[i] + (0.1441 - 1.0069 * SG[i]) * 1e5 / Tb[i]
 
-	def calc_Tc(SG, Tb):
-		# This function implements Lee-Kesler method for calculating Tc
-		Tc = np.empty(len(SG))
-		for i in range(len(SG)):
-			Tc[i] = 189.8 + 450.6* SG[i] + (0.4244 + 0.1174*SG[i])*Tb[i] + (0.1441 - 1.0069 * SG[i]) * 1e5 / Tb[i]
+	return Tc
 
-		return Tc
+def calc_Pc(SG, Tb):
+	# This function implements Lee-Kesler method for calculating Pc
+	Pc = np.empty(len(SG))
+	for i in range(len(SG)):
+		Pc[i] = exp(5.689 - 0.0566/SG[i] - (0.43639 + 4.1216 /SG[i] + 0.21343/SG[i]**2)* 1e-3*Tb[i] \
+				+ (0.47579 + 1.182/SG[i] + 0.15302/SG[i]**2)*1e-6*Tb[i]**2\
+				- (2.4505 + 9.9099/SG[i]**2) * 1e-10 * Tb[i]**3)
 
-	def calc_Pc(SG, Tb):
-		# This function implements Lee-Kesler method for calculating Pc
-		Pc = np.empty(len(SG))
-		for i in range(len(SG)):
-			Pc[i] = exp(5.689 - 0.0566/SG[i] - (0.43639 + 4.1216 /SG[i] + 0.21343/SG[i]**2)* 1e-3*Tb[i] \
-					+ (0.47579 + 1.182/SG[i] + 0.15302/SG[i]**2)*1e-6*Tb[i]**2\
-					- (2.4505 + 9.9099/SG[i]**2) * 1e-10 * Tb[i]**3)
+	return Pc
 
-		return Pc
+def calc_AF(Pc, Tc, Tb, Kw):
+	AF = np.empty(len(Pc))
+	for i in range(len(Pc)):
+		Tbr = Tb[i]/Tc[i]
+		if Tbr <= 0.8:
+			AF[i] = (-log(Pc[i]/1.01325) - 5.92714 + 6.09648/Tbr + 1.28862*log(Tbr) - 0.169347 * Tbr**6)/\
+					(15.2518 - 15.6875/Tbr - 13.4721 * log(Tbr) + 0.43577 * Tbr**6)
 
-	def calc_AF(Pc, Tc, Tb, Kw):
-		AF = np.empty(len(Pc))
-		for i in range(len(Pc)):
-			Tbr = Tb[i]/Tc[i]
-			if Tbr <= 0.8:
-				AF[i] = (-log(Pc[i]/1.01325) - 5.92714 + 6.09648/Tbr + 1.28862*log(Tbr) - 0.169347 * Tbr**6)/\
-						(15.2518 - 15.6875/Tbr - 13.4721 * log(Tbr) + 0.43577 * Tbr**6)
+		elif Tbr > 0.8:
+			AF[i] = -7.904 + 0.1352* Kw - 0.007465*Kw**2 + 8.359*Tbr + (1.408 - 0.01063 * Kw)/Tbr
 
-			elif Tbr > 0.8:
-				AF[i] = -7.904 + 0.1352* Kw - 0.007465*Kw**2 + 8.359*Tbr + (1.408 - 0.01063 * Kw)/Tbr
+	return AF
 
-		return AF
+def characterization(SG, x_data, y_data):
+	'''
+	Function for characterizing TBP data which creates pseudo cuts and returns Ti, Tf, W%, V%, M%, MW, SG and Tb of pseudo cuts
+	:param SG: Specific gravity of whole crude
+	:param x_data: list of Weight percent data points
+	:param y_data: list of Temperature data points in deg C
+	:return: lists of Ti(K), Tf(K), W%, V%, M%, MW, SG and Tb(K)
+	'''
+	for i in range(len(y_data)):
+		y_data[i] = (y_data[i] + 273.15)
 
-class characterization:
+	print(y_data)
+	P = np.linspace(0, y_data[0], 100)
 
-	def characterization(SG, x_data, y_data):
-		'''
-		Function for characterizing TBP data which creates pseudo cuts and returns Ti, Tf, W%, V%, M%, MW, SG and Tb of pseudo cuts
-		:param SG: Specific gravity of whole crude
-		:param x_data: list of Weight percent data points
-		:param y_data: list of Temperature data points in deg C
-		:return: lists of Ti(K), Tf(K), W%, V%, M%, MW, SG and Tb(K)
-		'''
-		for i in range(len(y_data)):
-			y_data[i] = (y_data[i] + 273.15)
+	rms_error = math.inf
+	Pf = 0
+	fitA = 0
+	fitB = 0
+	E = []
 
-		print(y_data)
-		P = np.linspace(0, y_data[0], 100)
+	for i in range(len(P)):
+		P0 = P[i]
 
-		rms_error = math.inf
-		Pf = 0
-		fitA = 0
-		fitB = 0
-		E = []
-
-		for i in range(len(P)):
-			P0 = P[i]
-
-			def fun(x,A, B):
-				'''function of versatile correlation using P0'''
-				y = np.empty(len(x))
-				for i in range(len(x)):
-					y[i] = P0 * (((A/B)*log(1/(1 - x[i]/100)))**B + 1)
-
-				return y
-
-			parameters, covariance = curve_fit(fun, x_data, y_data)
-			A = parameters[0]
-			B = parameters[1]
-			error = np.empty(len(x_data))
-			for i in range(len(x_data)):
-				error[i] = abs(y_data[i] - fun([x_data[i]], A, B))[0]
-			E.append(np.sqrt(np.mean(error**2)))
-			if rms_error > np.sqrt(np.mean(error**2)):
-				Pf = P0
-				rms_error = np.sqrt(np.mean(error ** 2))
-				fitA = A
-				fitB = B
-
-		def fun1(y, A, B):
-			'''inverse of versatile correlation using Pf'''
-			x = np.empty(len(y))
-			for i in range(len(y)):
-				x[i] = 1 - exp(-B/A * ((y[i] - Pf)/Pf)**(1/B))
-			return x
-
-		def fun2(x, A, B):
-			'''function of versatile correlation using Pf'''
+		def fun(x,A, B):
+			'''function of versatile correlation using P0'''
 			y = np.empty(len(x))
 			for i in range(len(x)):
-				y[i] = Pf * (((A / B) * log(1 / (1 - x[i] / 100))) ** B + 1)
+				y[i] = P0 * (((A/B)*log(1/(1 - x[i]/100)))**B + 1)
+
 			return y
 
-		IBP = fun2([0], fitA, fitB)[0]
-		FBP = fun2([99.99], fitA, fitB)[0]
+		parameters, covariance = curve_fit(fun, x_data, y_data)
+		A = parameters[0]
+		B = parameters[1]
+		error = np.empty(len(x_data))
+		for i in range(len(x_data)):
+			error[i] = abs(y_data[i] - fun([x_data[i]], A, B))[0]
+		E.append(np.sqrt(np.mean(error**2)))
+		if rms_error > np.sqrt(np.mean(error**2)):
+			Pf = P0
+			rms_error = np.sqrt(np.mean(error ** 2))
+			fitA = A
+			fitB = B
 
-		MeABP = (IBP + FBP)/2
+	def fun1(y, A, B):
+		'''inverse of versatile correlation using Pf'''
+		x = np.empty(len(y))
+		for i in range(len(y)):
+			x[i] = 1 - exp(-B/A * ((y[i] - Pf)/Pf)**(1/B))
+		return x
 
-		data = create_cuts(IBP, FBP)
+	def fun2(x, A, B):
+		'''function of versatile correlation using Pf'''
+		y = np.empty(len(x))
+		for i in range(len(x)):
+			y[i] = Pf * (((A / B) * log(1 / (1 - x[i] / 100))) ** B + 1)
+		return y
 
-		data["cut T"] = data["cuts_f"] - 273.15
-		data["Tb"] = (data["cuts_i"] + data["cuts_f"])/2
-		MeABP1 = FBP
-		Kw = MeABP ** (1 / 3) / SG
-		data["SG"] = data["Tb"] ** (1 / 3) / Kw
+	IBP = fun2([0], fitA, fitB)[0]
+	FBP = fun2([99.99], fitA, fitB)[0]
 
-		while abs(MeABP - MeABP1) > 0.01:
+	MeABP = (IBP + FBP)/2
 
-			MeABP = MeABP1
-			data["wt%"] = fun1(data["Tb"], fitA, fitB)
-			# Checking material balance consistency
-			data["cut wt%"] = data["wt%"].diff().fillna(data["wt%"])
+	data = create_cuts(IBP, FBP)
 
-			SG_t = 1 / sum(data["cut wt%"] / data["SG"])
+	data["cut T"] = data["cuts_f"] - 273.15
+	data["Tb"] = (data["cuts_i"] + data["cuts_f"])/2
+	MeABP1 = FBP
+	Kw = MeABP ** (1 / 3) / SG
+	data["SG"] = data["Tb"] ** (1 / 3) / Kw
 
-			# Normalizing denisty
+	while abs(MeABP - MeABP1) > 0.01:
 
-			data["SG"] = data["SG"] * SG / SG_t
+		MeABP = MeABP1
+		data["wt%"] = fun1(data["Tb"], fitA, fitB)
+		# Checking material balance consistency
+		data["cut wt%"] = data["wt%"].diff().fillna(data["wt%"])
 
-			data["MW"] = calc_MW(data["Tb"], data["SG"])
+		SG_t = 1 / sum(data["cut wt%"] / data["SG"])
 
-			V = np.empty(len(data.index))
+		# Normalizing denisty
 
-			V[0] = data.at[0,"wt%"]/data.at[0, "SG"]
+		data["SG"] = data["SG"] * SG / SG_t
 
-			V[1:] = [(data.at[i,"wt%"]-data.at[i-1, "wt%"])/data.at[i, "SG"] for i in range(1,len(data.index))]
+		data["MW"] = calc_MW(data["Tb"], data["SG"])
 
-			V = data["wt%"].diff().fillna(data["wt%"])/data["SG"]
+		V = np.empty(len(data.index))
 
-			V = V/sum(V)
-			data["V%"] = V.cumsum()
+		V[0] = data.at[0,"wt%"]/data.at[0, "SG"]
 
-			M = data["wt%"].diff().fillna(data["wt%"])/data["MW"]
+		V[1:] = [(data.at[i,"wt%"]-data.at[i-1, "wt%"])/data.at[i, "SG"] for i in range(1,len(data.index))]
 
-			M = M/sum(M)
-			data["M%"] = M.cumsum()
+		V = data["wt%"].diff().fillna(data["wt%"])/data["SG"]
 
-			MABP = sum(data["M%"].diff().fillna(data["M%"]) * data["Tb"])
-			CABP = (sum(data["V%"].diff().fillna(data["V%"]) * data["Tb"]**(1/3)))**3
+		V = V/sum(V)
+		data["V%"] = V.cumsum()
 
-			MeABP1 = (MABP + CABP)/2
-			Kw = MeABP1 ** (1 / 3) / SG
-			data["SG"] = (data["Tb"])**(1/3) / Kw
+		M = data["wt%"].diff().fillna(data["wt%"])/data["MW"]
 
-		Ti = list(data["cuts_i"])
-		Tf = list(data["cuts_f"])
-		Wf = list(data["wt%"])
-		Vf = list(data["V%"])
-		Mf = list(data["M%"])
-		MW = list(data["MW"])
-		SG = list(data["SG"])
-		Tb = list(data["Tb"])
+		M = M/sum(M)
+		data["M%"] = M.cumsum()
 
-		return [Ti, Tf, Wf, Vf, Mf, MW, SG, Tb]
+		MABP = sum(data["M%"].diff().fillna(data["M%"]) * data["Tb"])
+		CABP = (sum(data["V%"].diff().fillna(data["V%"]) * data["Tb"]**(1/3)))**3
 
-	# uncomment the code to see use of function
-	# SG = 0.809
-	#
-	# x = [2.9, 7.68859754431644, 15.1538050381994, 31.5823352252608, 42.4062967988428, 52.8464767354692, 62.7377406502837, 71.2394217843979, 74.0267929538375, 83.4788183586731, 88.4543077863666, 92.486636460126]
-	# y = [15, 65, 100, 150, 200, 250, 300, 350, 370, 450, 500, 550]
-	#
-	# ti, tf, wf, vf, mf, mw, sg, tb = characterization(SG, x, y)
-	# print(ti)
-	# print(tf)
-	# print(wf)
-	# print(vf)
-	# print(mf)
-	# print(mw)
-	# print(sg)
-	# print(tb)
+		MeABP1 = (MABP + CABP)/2
+		Kw = MeABP1 ** (1 / 3) / SG
+		data["SG"] = (data["Tb"])**(1/3) / Kw
+
+	Ti = list(data["cuts_i"])
+	Tf = list(data["cuts_f"])
+	Wf = list(data["wt%"])
+	Vf = list(data["V%"])
+	Mf = list(data["M%"])
+	MW = list(data["MW"])
+	SG = list(data["SG"])
+	Tb = list(data["Tb"])
+
+	return [Ti, Tf, Wf, Vf, Mf, MW, SG, Tb]
+
+# uncomment the code to see use of function
+# SG = 0.809
+#
+# x = [2.9, 7.68859754431644, 15.1538050381994, 31.5823352252608, 42.4062967988428, 52.8464767354692, 62.7377406502837, 71.2394217843979, 74.0267929538375, 83.4788183586731, 88.4543077863666, 92.486636460126]
+# y = [15, 65, 100, 150, 200, 250, 300, 350, 370, 450, 500, 550]
+#
+# ti, tf, wf, vf, mf, mw, sg, tb = characterization(SG, x, y)
+# print(ti)
+# print(tf)
+# print(wf)
+# print(vf)
+# print(mf)
+# print(mw)
+# print(sg)
+# print(tb)
